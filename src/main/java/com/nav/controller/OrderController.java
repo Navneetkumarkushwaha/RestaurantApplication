dda5448821a5480f1d028880defcab0e39e0d7ae
Navneetkumarkushwaha <navneet05082002kushwaha@gmail.com>
2025-03-22 01:30:39 +0530

    "

--- src/main/java/com/nav/controller/OrderController.java
+++ src/main/java/com/nav/controller/OrderController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 public class OrderController {
     private final OrderService orderService;
-
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
